5bc83b358fe642e18b30744bb2062398d8e6fd27
Jackson Ludwig <jacksonludwig0@gmail.com>
2021-10-29 14:59:26 -0400

    add split binds in kitty for nixos

--- modules/user/kitty/default.nix
+++ modules/user/kitty/default.nix
@@ -13,6 +13,22 @@
       map ctrl+shift+t new_tab_with_cwd
       touch_scroll_multiplier 2.0
       enable_audio_bell no
+
+      enabled_layouts splits
+
+      map F5 launch --location=hsplit
+      map F6 launch --location=vsplit
+      map F7 layout_action rotate
+
+      map shift+up move_window up
+      map shift+left move_window left
+      map shift+right move_window right
+      map shift+down move_window down
+
+      map ctrl+left neighboring_window left
+      map ctrl+right neighboring_window right
+      map ctrl+up neighboring_window up
+      map ctrl+down neighboring_window down
     '';
   };
 }
