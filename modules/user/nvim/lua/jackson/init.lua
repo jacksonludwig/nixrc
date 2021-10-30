449f33739505468f42d79fc8866291c80d992318
Jackson Ludwig <jacksonludwig0@gmail.com>
2021-10-29 23:58:07 -0400

    try lualine

--- modules/user/nvim/lua/jackson/init.lua
+++ modules/user/nvim/lua/jackson/init.lua
@@ -45,163 +45,63 @@ packer.startup(function()
   })
 
   use({
-    "NTBBloodbath/galaxyline.nvim",
+    "nvim-lualine/lualine.nvim",
     config = function()
-      local gl = require("galaxyline")
-      local condition = require("galaxyline.condition")
-      local gls = gl.section
-
-      local custom_filename_sect = {
-        provider = function()
-          local filename = vim.fn.expand("%:@")
-
-          return filename == "" and "[No Name]" or filename
-        end,
-        separator = " ",
-      }
-
-      gls.left[1] = {
-        FileIcon = {
-          provider = function()
-            local ft = vim.bo.filetype
-            return "[" .. (ft ~= "" and ft or "none") .. "]"
-          end,
-          condition = condition.buffer_not_empty,
-          highlight = "Comment",
-          separator = " ",
+      require("lualine").setup({
+        options = {
+          icons_enabled = false,
+          theme = "gruvbox",
+          component_separators = { left = "", right = "" },
+          section_separators = { left = "", right = "" },
+          disabled_filetypes = {},
+          always_divide_middle = true,
         },
-      }
-      gls.left[2] = {
-        CustomBufferFilename = custom_filename_sect,
-      }
-      gls.left[3] = {
-        FileStatus = {
-          provider = function()
-            if vim.bo.readonly == true then
-              if vim.bo.filetype == "help" then
-                return "[H] "
-              else
-                return "[RO] "
-              end
-            end
-
-            if vim.bo.modifiable and vim.bo.modified then
-              return "[+] "
-            end
-
-            return ""
-          end,
-          highlight = "Identifier",
+        sections = {
+          lualine_a = {},
+          lualine_b = { "branch", { "diagnostics", sources = { "nvim_lsp" } } },
+          lualine_c = { { "filename", path = 1 } },
+          lualine_x = { "filetype" },
+          lualine_y = { "progress" },
+          lualine_z = { "location" },
         },
-      }
-      gls.left[4] = {
-        LineInfo = {
-          provider = function()
-            local line = vim.fn.line(".")
-            local column = vim.fn.col(".")
-            return string.format("%d:%d ", line, column)
-          end,
-          condition = condition.buffer_not_empty,
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = { "filename" },
+          lualine_x = { "location" },
+          lualine_y = {},
+          lualine_z = {},
         },
-      }
-      gls.left[5] = {
-        LinePercent = {
-          provider = "LinePercent",
-          condition = condition.buffer_not_empty,
-        },
-      }
-
-      gls.right[1] = {
-        DiagnosticError = {
-          provider = "DiagnosticError",
-          icon = "E",
-          highlight = "DiagnosticError",
-        },
-      }
-      gls.right[2] = {
-        DiagnosticWarn = {
-          provider = "DiagnosticWarn",
-          icon = "W",
-          highlight = "DiagnosticWarn",
-        },
-      }
-      gls.right[3] = {
-        DiagnosticHint = {
-          provider = "DiagnosticHint",
-          icon = "H",
-          highlight = "DiagnosticHint",
-        },
-      }
-      gls.right[4] = {
-        DiagnosticInfo = {
-          provider = "DiagnosticInfo",
-          icon = "I",
-          highlight = "DiagnosticInfo",
-        },
-      }
-      gls.right[5] = {
-        lsp_status = {
-          provider = function()
-            local bufnr = vim.api.nvim_get_current_buf()
-            local clients = vim.lsp.buf_get_clients(bufnr)
-            local clientnames_tbl = {}
-
-            for _, v in pairs(clients) do
-              if v.name then
-                table.insert(clientnames_tbl, v.name)
-              end
-            end
-
-            if #clientnames_tbl ~= 0 then
-              return "lsp:" .. table.concat(clientnames_tbl, "/")
-            end
-
-            return ""
-          end,
-          highlight = "Comment",
-        },
-      }
-      gls.right[6] = {
-        GitIcon = {
-          provider = function()
-            return "git:"
-          end,
-          separator = " ",
-          condition = condition.check_git_workspace,
-          highlight = "Comment",
-        },
-      }
-      gls.right[7] = {
-        GitBranch = {
-          provider = "GitBranch",
-          condition = condition.check_git_workspace,
-          highlight = "Comment",
-        },
-      }
-
-      gls.short_line_left[1] = {
-        CustomBufferFilename = custom_filename_sect,
-      }
+        tabline = {},
+        extensions = {},
+      })
     end,
   })
 
   use({
-    "tjdevries/gruvbuddy.nvim",
-    requires = {
-      "tjdevries/colorbuddy.vim",
-    },
+    "sainnhe/gruvbox-material",
     config = function()
-      require("colorbuddy").colorscheme("gruvbuddy")
-      vim.cmd([[
-        hi MatchParen gui=underline
-        hi StatusLine guibg=NONE
-        hi StatusLine guifg=NONE
-        hi StatusLineNC guibg=NONE
-        hi VertSplit guifg=bg
-        hi VertSplit guibg=darkgrey
-      ]])
+      vim.cmd("colorscheme gruvbox-material")
     end,
   })
+
+  -- use({
+  --   "tjdevries/gruvbuddy.nvim",
+  --   requires = {
+  --     "tjdevries/colorbuddy.vim",
+  --   },
+  --   config = function()
+  --     require("colorbuddy").colorscheme("gruvbuddy")
+  --     vim.cmd([[
+  --       hi MatchParen gui=underline
+  --       hi StatusLine guibg=NONE
+  --       hi StatusLine guifg=NONE
+  --       hi StatusLineNC guibg=NONE
+  --       hi VertSplit guifg=bg
+  --       hi VertSplit guibg=darkgrey
+  --     ]])
+  --   end,
+  -- })
 
   use({
     "nvim-telescope/telescope.nvim",
