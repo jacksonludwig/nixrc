8db50727a5454c999cf4fe6caee46114fd537a38
Jackson Ludwig <jacksonludwig0@gmail.com>
2021-10-31 23:00:43 -0400

    try to use md instead of org for a lil

--- modules/user/nvim/lua/jackson/init.lua
+++ modules/user/nvim/lua/jackson/init.lua
@@ -24,17 +24,6 @@ packer.startup(function()
   })
 
   use({
-    "kristijanhusak/orgmode.nvim",
-    branch = "tree-sitter",
-    config = function()
-      require("orgmode").setup({
-        org_agenda_files = { "~/Dropbox/org/*" },
-        org_default_notes_file = "~/Dropbox/notes.org",
-      })
-    end,
-  })
-
-  use({
     "LnL7/vim-nix",
     ft = { "nix" },
   })
@@ -199,29 +188,16 @@ packer.startup(function()
   use({
     "nvim-treesitter/nvim-treesitter",
     config = function()
-      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-      parser_config.org = {
-        install_info = {
-          url = "https://github.com/milisims/tree-sitter-org",
-          revision = "main",
-          files = { "src/parser.c", "src/scanner.cc" },
-        },
-        filetype = "org",
-      }
-
       require("nvim-treesitter.configs").setup({
         highlight = {
           enable = true,
-          disable = { "org" },
           additional_vim_regex_highlighting = {
             "javascript",
             "javascriptreact",
             "typescript",
             "typescriptreact",
-            "org",
           },
         },
-        ensure_installed = { "org" },
       })
     end,
   })
