df838c4cbda8785f7c98b49af2e2358ff2aa3cf6
Jackson Ludwig <jacksonludwig0@gmail.com>
2021-10-31 22:37:56 -0400

    try org mode TS highlighting

--- modules/user/nvim/lua/jackson/init.lua
+++ modules/user/nvim/lua/jackson/init.lua
@@ -47,6 +47,16 @@ packer.startup(function()
   })
 
   use({
+    "kristijanhusak/orgmode.nvim",
+    config = function()
+      require("orgmode").setup({
+        org_agenda_files = { "~/Dropbox/org/*" },
+        org_default_notes_file = "~/Dropbox/notes.org",
+      })
+    end,
+  })
+
+  use({
     "nvim-lualine/lualine.nvim",
     requires = { "arkav/lualine-lsp-progress" },
     config = function()
@@ -199,11 +209,28 @@ packer.startup(function()
   use({
     "nvim-treesitter/nvim-treesitter",
     config = function()
+      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+      parser_config.org = {
+        install_info = {
+          url = "https://github.com/milisims/tree-sitter-org",
+          revision = "main",
+          files = { "src/parser.c", "src/scanner.cc" },
+        },
+        filetype = "org",
+      }
+
       require("nvim-treesitter.configs").setup({
         highlight = {
           enable = true,
-          additional_vim_regex_highlighting = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+          additional_vim_regex_highlighting = {
+            "javascript",
+            "javascriptreact",
+            "typescript",
+            "typescriptreact",
+            "org",
+          },
         },
+        ensure_installed = { "org" },
       })
     end,
   })
