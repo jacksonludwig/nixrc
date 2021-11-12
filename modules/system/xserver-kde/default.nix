dabd96f2320606fef8724dffcab4b5637b2234f8
Jackson Ludwig <jacksonludwig0@gmail.com>
2021-11-12 17:17:04 -0500

    kde conf

--- modules/system/xserver-kde/default.nix
+++ modules/system/xserver-kde/default.nix
@@ -1,7 +1,7 @@
-{ config, pkgs, ... }:
-{
+{ config, pkgs, ... }: {
   services.xserver.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
+  environment.systemPackages = with pkgs; [ kde-gtk-config ];
 
   networking.networkmanager.enable = true;
 }
