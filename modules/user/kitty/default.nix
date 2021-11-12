3c287070759d921ad46bde8b19200f825835c121
Jackson Ludwig <jacksonludwig0@gmail.com>
2021-11-11 21:36:09 -0500

    use same conf for both kitty confs

--- modules/user/kitty/default.nix
+++ modules/user/kitty/default.nix
@@ -1,35 +1,11 @@
-{ pkgs, config, lib, ... }: {
+{ pkgs, config, lib, modPath, linkConfig, inputs, ... }: 
+let
+  mkLink = linkConfig config;
+  confRoot = "${modPath.user}/kitty";
+in {
   programs.kitty = {
     enable = true;
-    extraConfig = ''
-      font_family JetBrainsMono Nerd Font
-      # font_family Iosevka
-      bold_font auto
-      bold_italic_font auto
-      italic_font auto
-
-      font_size 12
-
-      disable_ligatures always
-      map ctrl+shift+t new_tab_with_cwd
-      touch_scroll_multiplier 2.0
-      enable_audio_bell no
-
-      enabled_layouts splits
-
-      map F5 launch --location=hsplit --cwd=current
-      map F6 launch --location=vsplit --cwd=current
-      map F7 layout_action rotate
-
-      map shift+up move_window up
-      map shift+left move_window left
-      map shift+right move_window right
-      map shift+down move_window down
-
-      map ctrl+left neighboring_window left
-      map ctrl+right neighboring_window right
-      map ctrl+up neighboring_window up
-      map ctrl+down neighboring_window down
-    '';
   };
+
+  xdg.configFile."kitty/kitty.conf".source = mkLink.to "${confRoot}/kitty.conf";
 }
