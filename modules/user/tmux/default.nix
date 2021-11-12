32ef3db870e1ddfdc277a9a1c4f61e6336f3c950
Jackson Ludwig <jacksonludwig0@gmail.com>
2021-11-11 23:29:10 -0500

    copy to system in tmux with xsel

--- modules/user/tmux/default.nix
+++ modules/user/tmux/default.nix
@@ -1,4 +1,7 @@
 { pkgs, config, lib, linkConfig, modPath, ... }: {
+  home.packages = with pkgs; [
+    xsel
+  ];
   home.file.".tmux.conf".source =
     (linkConfig config).to "${modPath.user}/tmux/.tmux.conf";
 }
