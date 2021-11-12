0b16b79f4969b562ac1122ce85883cc2109bad9e
Jackson Ludwig <jacksonludwig0@gmail.com>
2021-11-11 21:30:18 -0500

    try gnome extension

--- modules/system/xserver-gnome40/default.nix
+++ modules/system/xserver-gnome40/default.nix
@@ -3,4 +3,6 @@
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+
+  environment.systemPackages = with pkgs; [ gnomeExtensions.appindicator ];
 }
