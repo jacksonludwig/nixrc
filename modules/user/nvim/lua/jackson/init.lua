ec7a1e2c544d5729cfcc54c9ee7a40bc1fdcb5d4
Jackson Ludwig <jacksonludwig0@gmail.com>
2021-11-18 07:58:15 -0500

    eslint deprecated from ts-utils

--- modules/user/nvim/lua/jackson/init.lua
+++ modules/user/nvim/lua/jackson/init.lua
@@ -326,21 +326,7 @@ packer.startup(function()
           local ts_utils = require("nvim-lsp-ts-utils")
 
           ts_utils.setup({
-            import_all_scan_buffers = 100,
-
-            -- eslint
-            eslint_bin = "eslint_d",
-            eslint_enable_diagnostics = false,
-            eslint_enable_disable_comments = false,
-            eslint_enable_code_actions = false,
-
-            -- formatting
-            enable_formatting = false,
-            formatter = "eslint_d",
-
-            -- update imports on file move
-            update_imports_on_move = true,
-
+            enable_import_on_completion = true,
             filter_out_diagnostics_by_code = { 80001 },
             filter_out_diagnostics_by_severity = { "hint" },
           })
