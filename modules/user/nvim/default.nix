5e06070472c8eeaa506574f105a2006e2f9743e3
Jackson Ludwig <jacksonludwig0@gmail.com>
2021-12-07 17:45:00 -0500

    remove treesitter stuff from nvim .nix

--- modules/user/nvim/default.nix
+++ modules/user/nvim/default.nix
@@ -1,42 +1,25 @@
 { pkgs, config, lib, linkConfig, modPath, inputs, ... }@args:
 let
   mkLink = linkConfig config;
-  ts = pkgs.tree-sitter.builtGrammars;
   confRoot = "${modPath.user}/nvim";
 
   languageServers = with pkgs; [
     typescript-language-server
     nodePackages.vscode-langservers-extracted
-    nodePackages.prettier
     nodePackages.yaml-language-server
     nodePackages.pyright
     sumneko-lua-language-server
   ];
 
   formatters = with pkgs; [
-    tree-sitter
     stylua
     nixfmt
     black
     python39Packages.isort
-    fd
-    neovim-remote
+    nodePackages.prettier
   ];
 
-  # (xdg.configFile's)
-  treesitterParsers = {
-    # "nvim/parser/c.so".source = "${ts.tree-sitter-c}/parser";
-    # "nvim/parser/lua.so".source = "${ts.tree-sitter-lua}/parser";
-    # "nvim/parser/rust.so".source = "${ts.tree-sitter-rust}/parser";
-    # "nvim/parser/python.so".source = "${ts.tree-sitter-python}/parser";
-    # "nvim/parser/typescript.so".source = "${ts.tree-sitter-typescript}/parser";
-    # "nvim/parser/javascript.so".source = "${ts.tree-sitter-javascript}/parser";
-    # "nvim/parser/tsx.so".source = "${ts.tree-sitter-tsx}/parser";
-    # "nvim/parser/nix.so".source = "${ts.tree-sitter-nix}/parser";
-    # "nvim/parser/yaml.so".source = "${ts.tree-sitter-yaml}/parser";
-    # "nvim/parser/bash.so".source = "${ts.tree-sitter-bash}/parser";
-    # "nvim/parser/comment.so".source = "${ts.tree-sitter-comment}/parser";
-  };
+  tools = with pkgs; [ tree-sitter fd neovim-remote ];
 
 in {
   programs.bash = {
@@ -45,7 +28,7 @@ in {
     '';
   };
 
-  home.packages = with pkgs; [ ] ++ formatters ++ languageServers;
+  home.packages = with pkgs; [ ] ++ formatters ++ languageServers ++ tools;
 
   programs.neovim = {
     enable = true;
@@ -60,17 +43,16 @@ in {
     '';
   };
 
-  # Do the requisite black magic for telescope-fzf
+  # Install the fzf shared library
   xdg.dataFile."nvim/site/pack/packer/start/telescope-fzf-native.nvim/build/libfzf.so".source =
     "${pkgs.telescope-fzf-native}/build/libfzf.so";
 
-  #
   # Actual config files
-  #
   xdg.configFile = {
-    "nvim/lua/jackson/init.lua".source = mkLink.to "${confRoot}/lua/jackson/init.lua";
+    "nvim/lua/jackson/init.lua".source =
+      mkLink.to "${confRoot}/lua/jackson/init.lua";
     "nvim/coc/coc.vim".source = mkLink.to "${confRoot}/coc/coc.vim";
     "nvim/ftplugin/markdown.vim".source =
       mkLink.to "${confRoot}/ftplugin/markdown.vim";
-  } // treesitterParsers;
+  };
 }
